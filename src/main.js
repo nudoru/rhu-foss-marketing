f1772e90dcbd69cfec163c9bca53bc0dcaa85131
Matt Perkins <hello@mattperkins.me>
2020-07-24 13:34:41 -0400

    added purgecss

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import VueDummy from 'vue-dummy';
+//import VueDummy from 'vue-dummy';
 import App from './App.vue';
 // import router from './router';
 // import store from './store';
 import '@/scss/styles.scss';
 import '@/components/globals';
 
-Vue.use(VueDummy);
+//Vue.use(VueDummy);
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
